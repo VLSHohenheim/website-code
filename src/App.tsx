2ce42e3935adb3885eb6262ab463e358ed8411b6
VLSHohenheim <vls.hohenheim@gmail.com>
2025-02-21 23:41:39 +0100

    Image change

--- src/App.tsx
+++ src/App.tsx
@@ -133,7 +133,7 @@ function App() {
         id="contact"
         className="min-h-screen bg-fixed bg-cover bg-center"
         style={{
-          backgroundImage: "url('https://images.unsplash.com/photo-1557425955-df376b5903c8?auto=format&fit=crop&w=1920')"
+          backgroundImage: "url('https://images.pexels.com/photos/774448/pexels-photo-774448.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')"
         }}
       >
         <div className="h-full py-20 flex items-center justify-center bg-white bg-opacity-90 dark:bg-gray-900 dark:bg-opacity-90">
