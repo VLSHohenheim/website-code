51f5d0b26d53f6f14926df5b9f2f2fde6a6e0ab2
VLSHohenheim <vls.hohenheim@gmail.com>
2025-02-21 23:44:54 +0100

    Image change

--- src/App.tsx
+++ src/App.tsx
@@ -41,7 +41,7 @@ function App() {
         id="welcome"
         className="h-screen bg-fixed bg-cover bg-center"
         style={{
-          backgroundImage: "url('https://images.pexels.com/photos/1366942/pexels-photo-1366942.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')"
+          backgroundImage: "url('https://images.pexels.com/photos/248152/pexels-photo-248152.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')"
         }}
       >
         <div className="h-full flex items-center justify-center bg-black bg-opacity-50">
