0a4e756c595f1bf47087998d5232ba7da685a7c3
VLSHohenheim <vls.hohenheim@gmail.com>
2025-04-19 01:36:13 +0200

    Update en.ts

--- src/i18n/en.ts
+++ src/i18n/en.ts
@@ -1,4 +1,4 @@
-// englische Übersetzungen 
+// englische Übersetzungen
 const en = {
   nav: {
     about: 'About Us',
@@ -26,7 +26,7 @@ const en = {
     excursion: {
       title: 'First Excursion: Visit to Ritter Sport, Waldenbuch',
       content:
-        'On April 9th 2025, we had our first company visit at Ritter Sport. We enjoyed exclusive insights into production lines, quality control and career opportunities at this iconic chocolate manufacturer.<br /><br />Below is a group photo of our VLS members in front of the production hall.',
+        'On April 9th 2025, we had our first company excursion at Ritter Sport. We enjoyed exclusive insights into production lines, quality control and career opportunities at this iconic chocolate manufacturer.<br /><br />Below is a group photo of our VLS members in front of the production hall.',
       imageAlt: 'Group photo of VLS Hohenheim during Ritter Sport excursion',
     },
   },
