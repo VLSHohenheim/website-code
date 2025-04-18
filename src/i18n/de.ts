8a4687f5b2811748285f833d24eb28693dd63f2f
VLSHohenheim <vls.hohenheim@gmail.com>
2025-04-19 01:24:01 +0200

    Update de.ts

--- src/i18n/de.ts
+++ src/i18n/de.ts
@@ -20,7 +20,7 @@ const de = {
     stadtradeln: {
       title: 'Stadtradeln 2025: Ultimate Frisbee Gang sponsored by VLS Hohenheim',
       content:
-        'Beim diesjährigen Stadtradeln ist das Ultimate Frisbee Team aus Hohenheim "Pulldoggs Hohenheim" unter neuem Namen am Start: <strong>Ultimate Frisbee Gang sponsored by VLS Hohenheim</strong>.<br /><br />Mit diesem Namenssponsoring zeigen wir unser Engagement für <strong>nachhaltige Mobilität</strong> und <strong>sportlichen Teamgeist</strong>. Ab dem 05. Mai radeln wir gemeinsam für bessere Luft, weniger Verkehr und mehr Zusammenhalt!<br /><br />Meldet euch jetzt an:',
+        'Beim diesjährigen Stadtradeln ist das Ultimate Frisbee Team aus Hohenheim "Pulldoggs Hohenheim" unter neuem Namen am Start: <strong>Ultimate Frisbee Gang sponsored by VLS Hohenheim</strong>.<br /><br />Mit diesem Namenssponsoring zeigen wir unser Engagement für <strong>nachhaltige Mobilität</strong> und <strong>sportlichen Teamgeist</strong>. Ab dem 05. Mai radeln wir gemeinsam für bessere Luft, weniger Verkehr und mehr Zusammenhalt!<br /><br />Meldet euch jetzt an:'
       button: 'Stadtradeln Anmeldung',  // Hinzugefügter Button-Text
     },
     excursion: {
