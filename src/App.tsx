d4d3ce7a826a8ed5e307372562d40a65358c42dd
VLSHohenheim <vls.hohenheim@gmail.com>
2025-04-19 23:16:31 +0200

    Undo App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -159,15 +159,15 @@ function App() {
       />
 
       {/* Willkommen-Sektion */}
-      <section id="welcome" className="min-h-screen bg-fixed bg-cover bg-center"
-        style={{ backgroundImage: "url('...')" }}>
-          <div className="h-full py-32 flex items-start justify-center bg-black bg-opacity-50">
-            <div className="text-center text-white">
-              <h1 className="text-5xl font-bold mb-4">{t('welcome.title')}</h1>
-              <p className="text-xl">{t('welcome.subtitle')}</p>
-            </div>
+      <section id="welcome" className="h-screen bg-fixed bg-cover bg-center"
+        style={{ backgroundImage: "url('https://images.pexels.com/photos/29465326/pexels-photo-29465326/free-photo-of-dark-food-photography-with-berries-and-nuts.jpeg')" }}>
+        <div className="h-full flex items-center justify-center bg-black bg-opacity-50">
+          <div className="text-center text-white">
+            <h1 className="text-5xl font-bold mb-4">{t('welcome.title')}</h1>
+            <p className="text-xl">{t('welcome.subtitle')}</p>
           </div>
-        </section>
+        </div>
+      </section>
 
       {/* Über-uns-Sektion */}
       <section id="about" className="min-h-screen bg-fixed bg-cover bg-center"
